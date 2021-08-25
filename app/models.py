ec1b13c28350ae5429bb1a739d468981edeae522
Awadh-Awadh <awadh.awadhawadh.awadh@student.moringaschool.com>
2021-08-26 02:00:45 +0300

    add load user function

--- app/models.py
+++ app/models.py
@@ -1,5 +1,9 @@
 from .import db
-from flask_login import UserMixin
+from flask_login import UserMixin, login_manager
+
+@login_manager.user_loaded
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
 class User(db.Model, UserMixin):
